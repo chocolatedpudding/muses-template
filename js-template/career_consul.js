985139c32acc9d38b170d326e1419aed434e985a
chocolatedpudding <166581633+chocolatedpudding@users.noreply.github.com>
2024-07-03 15:23:13 +0900

    情報取得について

--- js-template/career_consul.js
+++ js-template/career_consul.js
@@ -1,30 +1,29 @@
 "use strict";
-/*
-let elements = document.getElementsByName("nameRadio");
-let len = elements.length;
-let checkValue = "";
 
-for (let i = 0; i < len; i++) {
-  if (elements.item(i).checked) {
-    checkValue = elements.item(i).value;
-  }
-}
-*/
+const storage = localStorage; // ストレージをlocalStorageに設定
 
-function butotnClick() {
+//career_consul01のラジオボタン情報取得、数も取得
+const consulType = document.getElementsByName("careerRadio");
+let len = consulType.length;
+
+let checkButton = document.getElementById("nextButton");
+
+//次へボタンを押したら選択したラジオボタンの値を取得して、ローカルストレージに追加
+checkButton.addEventListener("click", () => {
   let checkValue = "";
-
   for (let i = 0; i < len; i++) {
-    if (careerRadio.item(i).checked) {
-      checkValue = careerRadio.item(i).value;
+    if (consulType.item(i).checked) {
+      checkValue = consulType.item(i).value;
     }
   }
-  console.log("選択されているのは " + checkValue + " です");
-}
 
-let careerRadio = document.getElementsByName("careerRadio");
-let len = careerRadio.length;
-careerRadio[0].checked = true;
+  storage.store = checkValue; // 'store'キーに入力値を記録
+});
 
-let checkButton = document.getElementById("checkButton");
-checkButton.addEventListener("click", butotnClick);
+//
+let typeText = document.getElementById("type");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const storagedData = storage.store; // ストレージデータの取得
+  output.textContent = storagedData; // div要素に出力
+});
