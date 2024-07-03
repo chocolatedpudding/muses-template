f2187efc5a2650083d188a2ff710d9b28bb23ee2
chocolatedpudding <166581633+chocolatedpudding@users.noreply.github.com>
2024-07-03 18:36:20 +0900

    相談内容OK

--- js-template/career_consul04.js
+++ js-template/career_consul04.js
@@ -4,12 +4,11 @@ const storage = localStorage; // ストレージをlocalStorageに設定
 
 //career_consul03のラジオボタン情報取得、数も取得
 const consulContent = document.getElementById("contentDrop");
-let len = consulContent.length;
 let checkButton = document.getElementById("nextButton04");
 
 //次へボタンを押したら選択したラジオボタンの値を取得して、ローカルストレージに追加
 checkButton.addEventListener("click", () => {
-  //let checkValue = dropContent.value;
+  let checkValue = consulContent.value;
   storage.setItem("contents", checkValue);
 
   storage.contents = checkValue; // 'contents'キーに入力値を記録
