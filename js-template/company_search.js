2da0c0c688ae0a61cc9d4008f19382b22e39049b
Audio002 <166581632+Audio002@users.noreply.github.com>
2024-07-12 11:20:20 +0900

    通称

--- js-template/company_search.js
+++ js-template/company_search.js
@@ -2,9 +2,12 @@ async function getCompanyInfo() {
   const companyName = document
     .getElementById("companyName")
     .value.toLowerCase();
+  const companyCommonName = document
+    .getElementById("companyCommonName")
+    .value.toLowerCase();
 
-  // 入力が空の場合はアラートを表示
-  if (!companyName) {
+  // 入力が両方とも空の場合はアラートを表示
+  if (!companyName && !companyCommonName) {
     alert("検索条件が入力されていません。検索条件を入力してください。");
     return;
   }
@@ -12,9 +15,18 @@ async function getCompanyInfo() {
   const response = await fetch("company.json");
   const data = await response.json();
 
-  const matchingCompanies = data.filter((company) =>
-    company.name.toLowerCase().includes(companyName)
-  );
+  const matchingCompanies = data.filter((company) => {
+    const nameMatch = company.name.toLowerCase().includes(companyName);
+    const commonMatch = company.common.toLowerCase() === companyCommonName;
+    if (companyName && companyCommonName) {
+      return nameMatch && commonMatch;
+    } else if (companyName) {
+      return nameMatch;
+    } else if (companyCommonName) {
+      return commonMatch;
+    }
+    return false;
+  });
 
   if (matchingCompanies.length > 0) {
     displayCompanyInfo(matchingCompanies);
